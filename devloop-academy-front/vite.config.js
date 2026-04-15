e5e805014421e96bc734ea2e49992a81e2f1083d
Miguel Hernandez <miguelx98@hotmail.com>
2026-04-14 22:19:26 -0500

    Setting para GH pages

--- devloop-academy-front/vite.config.js
+++ devloop-academy-front/vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  // Pon aquí el nombre exacto de tu repositorio entre barras
+  base: '/DevLoopAcademy/', 
 })
